9facdbd9f942bf7996097199820e3191a3fbf3ca
stevenpfaff <pfaffy1426@gmail.com>
2021-10-19 21:09:46 -0500

    added related videos label

--- src/components/RelatedVideos/RelatedVideos.js
+++ src/components/RelatedVideos/RelatedVideos.js
@@ -5,12 +5,14 @@ const RelatedVideos = (props) => {
     function videoLinks() {
         return props.relatedVideos.map((video) => {
             return (
+                
                     <li><a href={`http://www.youtube.com/watch?v=${video.videoId}`}>{video.videoTitle}</a></li>
                     );
         });
     }
         return (
             <div>
+                <h1> Related Videos :</h1>
                 {videoLinks()}
             </div>
         );
